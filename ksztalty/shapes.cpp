e61b79543036b7dedd909ffec87aae096be8a3bb
Roman Nowicki <rnowicki@sims.pl>
2012-12-19 23:28:09 +0100

    rysowanie podstawowych ksztaltow

--- ksztalty/shapes.cpp
+++ ksztalty/shapes.cpp
@@ -14,13 +14,49 @@ Shapes::~Shapes()
 
 void Shapes::initializeGL()
 {
+    glEnable(GL_DEPTH_TEST);
 }
 
 void Shapes::resizeGL(int w, int h)
 {
+    glViewport(0, 0, w, h);
+
+    glMatrixMode(GL_PROJECTION);
+
+    glLoadIdentity();
+    gluPerspective(45.0,
+                   (double)w / (double)h,
+                   1.0,
+                   10.0);
 }
 
 void Shapes::paintGL()
 {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_QUADS);
+    glVertex3f(-0.2f, -1.5f, -5.0f);
+    glVertex3f(0.2f, -1.5f, -5.0f);
+    glVertex3f(0.2f, -0.5f, -5.0f);
+    glVertex3f(-0.2f, -0.5f, -5.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0.5f, 0.5f, -5.0f);
+    glVertex3f(1.5f, 0.5f, -5.0f);
+    glVertex3f(0.5f, 1.0f, -5.0f);
+
+    glVertex3f(0.5f, 1.0f, -5.0f);
+    glVertex3f(1.5f, 1.0f, -5.0f);
+    glVertex3f(1.0f, 1.5f, -5.0f);
+
+    glVertex3f(0.5f, -0.5f, -5.0f);
+    glVertex3f(1.0f, -1.5f, -5.0f);
+    glVertex3f(1.5f, -0.5f, -5.0f);
+    glEnd();
+
+    swapBuffers();
 }
 
